e07df1a2746b4a2c678114041d86af138b31bd0a
Linda Seiter <linseiter@gmail.com>
2023-08-28 13:15:18 -0400

    remove solution from app.py

--- server/app.py
+++ server/app.py
@@ -23,32 +23,7 @@ def index():
     )
     return response
 
-
-@app.route('/pets/<int:id>')
-def pet_by_id(id):
-    pet = Pet.query.filter(Pet.id == id).first()
-
-    if pet:
-        response_body = f'<p>{pet.name} {pet.species}</p>'
-        response_status = 200
-    else:
-        response_body = f'<p>Pet {id} not found</p>'
-        response_status = 404
-
-    response = make_response(response_body, response_status)
-    return response
-
-
-@app.route('/species/<string:species>')
-def pet_by_species(species):
-    pets = Pet.query.filter_by(species=species).all()
-
-    size = len(pets)  # all() returns a list so we can get length
-    response_body = f'<h2>There are {size} {species}s</h2>'
-    for pet in pets:
-        response_body += f'<p>{pet.name}</p>'
-    response = make_response(response_body, 200)
-    return response
+# add routes here 
 
 
 if __name__ == '__main__':
